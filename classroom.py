bfd3cdb6f967117a49ddba265a54368520ed208e
guljigit-10 <guljigit-11>
2025-09-04 15:27:40 +0600

    биринчи

--- classroom.py
+++ classroom.py
@@ -2,3 +2,7 @@ class Students:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+
+class Adam(Students):
+    def __init__(self, name, age,):
+        super().__init__('Adilet', 37)
